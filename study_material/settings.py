b3007fff26bddf00b923a4b5f9324278eb2a512d
Harsh Jabade <harshjabade@example.com>
2026-05-17 17:06:30 +0530

    Fix 500 error: Change database fallback to sqlite

--- study_material/settings.py
+++ study_material/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'study_material.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'mysql://root:harsh@123@localhost:3306/study_material'),
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600
     )
 }
